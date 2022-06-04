02c9bd0c20381f4fd9cb3d5e6e66f97de9062a59
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-06-04 23:12:28 +0400

    Initial commit

---
+++ src/test/java/solutions/dmitrikonnov/einstufungstest/EinstufungsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package solutions.dmitrikonnov.einstufungstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinstufungsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
