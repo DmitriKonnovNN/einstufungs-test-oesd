64ce93b8220d1f5d4fb16a1d020e4fffec345e91
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-08-07 22:42:08 +0400

    deployment added

--- src/main/java/solutions/dmitrikonnov/einstufungstest/aws/s3/S3ConnectionConfig.java
+++ src/main/java/solutions/dmitrikonnov/einstufungstest/aws/s3/S3ConnectionConfig.java
@@ -2,6 +2,7 @@ package solutions.dmitrikonnov.einstufungstest.aws.s3;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.opencsv.CSVReader;
@@ -38,31 +39,35 @@ public class S3ConnectionConfig {
     //@Value("${aws.s3.access.key.secret}")
 
 
+
     @Value("${aws.s3.region.name}")
     private final String s3RegionName;
 
-    @Bean
-    public List<String> credentials (){
-        List<String> keyIdandSecret = null;
-        try {
-            CSVReader reader = new CSVReader(new FileReader(fileName));
-              keyIdandSecret = reader.readAll().stream()
-                    .skip(1)
-                    .flatMap(Arrays::stream)
-                    .collect(Collectors.toUnmodifiableList());
-            System.out.println(keyIdandSecret.toString());
+    //private final SystemPropertiesCredentialsProvider credPrvdr;
 
-
-        } catch (FileNotFoundException e) {
-            log.error("Could not read credentials from CSV. Process failed!");
-        } catch (IOException e) {
-            log.error(e.toString());
-            e.printStackTrace();
-        } catch (CsvException e) {
-            log.error(e.toString());
-        }
-        return keyIdandSecret;
-    }
+//    @Bean
+//    public List<String> credentials (){
+//
+//        List<String> keyIdandSecret = null;
+//        try {
+//            CSVReader reader = new CSVReader(new FileReader(fileName));
+//              keyIdandSecret = reader.readAll().stream()
+//                    .skip(1)
+//                    .flatMap(Arrays::stream)
+//                    .collect(Collectors.toUnmodifiableList());
+//            System.out.println(keyIdandSecret.toString());
+//
+//
+//        } catch (FileNotFoundException e) {
+//            log.error("Could not read credentials from CSV. Process failed!");
+//        } catch (IOException e) {
+//            log.error(e.toString());
+//            e.printStackTrace();
+//        } catch (CsvException e) {
+//            log.error(e.toString());
+//        }
+//        return keyIdandSecret;
+//    }
 
 
     @Bean
@@ -71,11 +76,11 @@ public class S3ConnectionConfig {
 
         }
 
-        final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(credentials().get(0), credentials().get(1));
+        //final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(credentials().get(0), credentials().get(1));
         // Get Amazon S3 client and return the S3 client object
         return AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                //.withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
                 .withRegion(s3RegionName)
                 .build();
     }
